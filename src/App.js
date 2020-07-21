d885caa091cd91e4de51172c3e9451b586107c95
gcmartin <gscalica@gmail.com>
2020-07-21 12:06:25 -0700

    Create 'components' folder and 'Box' component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Box from './components/Box';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Color Boxes Exercise</h1>
+      <Box />
     </div>
   );
 }
---
+++ src/components/Box.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Box() {
+  return(
+    <div>
+      This is a box.
+    </div>
+  );
+}
+
+export default Box;
